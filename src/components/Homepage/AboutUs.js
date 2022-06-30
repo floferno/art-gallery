a2a30c036e04a8b34b24ab0bd22c639ee904b6bd
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2022-06-30 23:44:24 +0700

    feat: add about us

---
+++ src/components/Homepage/AboutUs.js
@@ -0,0 +1,64 @@
+import Image from 'next/image'
+import React from 'react'
+import Grid from '../Common/Grid'
+
+const AboutUs = () => {
+  return (
+    <Grid className="mb-[120px]">
+      <div className="col-span-6 relative">
+        <Image
+          width={607}
+          height={458}
+          src="/homepage/about-us.png"
+          alt="About Us"
+          layout="responsive"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <div className="col-span-6 flex flex-col">
+        <div className="flex flex-col">
+          <h3 className="text-2xl font-semibold mb-3 tracking-wide">
+            About Us
+          </h3>
+          <p className="text-sm mb-8">
+            Inspired by a nomadic tribe that calls themselves the Amazigh (which
+            means free or noble people) in North Africa, Artmazigh team believes
+            that everyone is free and unique individual, and imagination is no
+            exception. Through the illustration works that we have done since
+            2011 and together with people who believe in the power of
+            imagination (Musicians, Politicians, Soldiers, Writers, Youtubers,
+            Gamers, Athletes and many more), we call on everyone to have brave
+            imaginations and go beyond the feeling of failing to make it happen.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <h4 className="text-lg font-semibold mb-1">Vision</h4>
+          <p className="text-sm mb-8">
+            To gain trust, love and respect from everyone by realizing every
+            imagination through works of illustration art
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <h4 className="text-lg font-semibold mb-1">Mission</h4>
+          <ul className="text-sm list-none list-outside ml-6">
+            <li className='before:content-["\2022"] before:text-white before:font-bold before:inline-block before:w-3 before:text-base before:absolute relative before:-ml-4'>
+              Doing illustration artworks based on freedom of expression and
+              professionalism,
+            </li>
+            <li className='before:content-["\2022"] before:text-white before:font-bold before:inline-block before:w-3 before:text-base before:absolute relative before:-ml-4'>
+              Becoming a space for imaginators (clients) and illustrators to
+              explore and communicate ideas and imaginations,
+            </li>
+            <li className='before:content-["\2022"] before:text-white before:font-bold before:inline-block before:w-3 before:text-base before:absolute relative before:-ml-4'>
+              Producing high-quality illustration arts by prioritizing the
+              values of professionalism and beauty.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Grid>
+  )
+}
+
+export default AboutUs
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,35 +2,20 @@
 
 // import Button from '@/components/Common/Button';
 import Container from '@/components/Common/Container'
-import Grid from '@/components/Common/Grid'
-import Gallery from '@/components/Homepage/Gallery'
+
+import AboutUs from '@/components/Homepage/AboutUs'
+
 import Jumbotron from '@/components/Homepage/Jumbotron'
 import Layout from '@/components/Layout/Layout'
 import gallery from '@/data/gallery'
 
 export default function Home() {
-  const imageSrc = ['/artworks/1.png', '/artworks/2.png', '/artworks/3.png']
-  console.log('gallery', gallery)
   return (
     <Layout>
       <Jumbotron />
       <Container className="mt-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod odio
-        consequatur debitis magni iste modi deserunt omnis eveniet laudantium
-        illum ab nulla illo vitae a, nam sequi eius! Ut, suscipit.
-        <div className="grid grid-cols-3">
-          {gallery.map((src, index) => {
-            return (
-              <div>
-                <img src={src.imgSrc} key={index} />
-              </div>
-            )
-          })}
-        </div>
+        <AboutUs />
       </Container>
-      {/* <Container className="mt-4">
-        <Gallery />
-      </Container> */}
     </Layout>
   )
 }
