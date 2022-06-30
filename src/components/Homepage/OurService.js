0cdd295e372fe10d9bf84b7a8158ff66ef877f13
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2022-06-30 23:46:02 +0700

    feat: init our service

---
+++ src/components/Homepage/OurService.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Grid from '../Common/Grid'
+
+const OurService = () => {
+  return (
+    <Grid>
+      <div className="col-span-3">
+        <h2>Our Service</h2>
+      </div>
+      <div className="col-span-full">
+        <button>NFT</button>
+        <button>Dark Art</button>
+        <button>Ilustration</button>
+        <button>Logo</button>
+      </div>
+    </Grid>
+  )
+}
+
+export default OurService
