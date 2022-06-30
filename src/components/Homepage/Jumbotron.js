8b7ce14fb52be5716cc35c9d98cf72835c631f09
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2022-06-30 23:44:06 +0700

    fix: jumbotron arrow

--- src/components/Homepage/Jumbotron.js
+++ src/components/Homepage/Jumbotron.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Container from '../Common/Container'
 import Grid from '../Common/Grid'
 import Button from '../Common/Button'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+
 const Jumbotron = () => {
   return (
-    <div className="w-full h-auto">
+    <div className="w-full h-auto mb-[100px]">
       <div className="flex flex-col">
         <div
-          className="w-full bg-cover bg-no-repeat"
+          className="w-full bg-cover bg-no-repeat mb-8"
           style={{ backgroundImage: `url('/homepage/slider.png')` }}
         >
           <div className="w-full h-full bg-slider ">
@@ -32,11 +34,21 @@ const Jumbotron = () => {
           </div>
         </div>
         {/* Slider Pagination */}
-        <div className="flex items-center justify-center mx-auto">
-          <button>Arrow Left</button>
-          <button>Bullet</button>
-          <button>Bullet</button>
-          <button>Arrow Right</button>
+        <div className="flex items-center justify-center mx-auto gap-4">
+          <button className="h-6 w-6 flex items-center justify-center">
+            <ChevronLeftIcon className="h-4 w-4 text-neutral-50 hover:text-brand-primary transition-all" />{' '}
+          </button>
+          <button>
+            <div className="h-3 w-3 bg-brand-secondary border border-brand-secondary rounded-full transition-all"></div>
+          </button>
+          <button>
+            <div className="h-3 w-3 bg-transparent border border-neutral-50 hover:bg-brand-secondary hover:border-brand-secondary  rounded-full  transition-all"></div>
+          </button>
+
+          <button>
+            {' '}
+            <ChevronRightIcon className="h-4 w-4 text-neutral-50 hover:text-brand-primary transition-all" />
+          </button>
         </div>
       </div>
     </div>
