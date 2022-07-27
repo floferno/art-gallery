391603ac317a13fe0d215f3aa7078785044f0794
floferno <olfiarachmi@gmail.com>
2022-07-27 08:23:58 +0800

    update footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,15 +3,20 @@ import Container from '../Common/Container'
 import Image from 'next/image'
 import Button from '../Common/Button'
 import Link from 'next/link'
+import FacebookIcons from '../../../public/icons/fb-icons.svg'
+import InstagramIcons from '../../../public/icons/ig-icons.svg'
+import FiverrIcons from '../../../public/icons/fiverr-icons.svg'
+
 
 const Footer = () => {
   return (
     <>
-      <div className="bg-brand-senary w-full ">
+      <div className="bg-brand-senary w-full h-[388px]">
         <Container className={'flex flex-col '}>
-          <div className="border text-white py-[63px] px-[34px] w-auto border-brand-primary rounded-lg relative  -mt-[135px]">
+          <div className="border text-white py-[63px] px-[34px] w-auto border-brand-primary bg-brand-quaternary rounded-lg relative  -mt-[135px]">
             <div className="flex items-end flex-col xl:flex-row">
               <h3 className="font-bold text-[36px] leading-relaxed xl:mr-2 mx-auto xl:mx-0">
+
                 Get informed for our latest update
               </h3>
 
@@ -51,7 +56,8 @@ const Footer = () => {
             </div>
           </div>
           <div className="grid grid-cols-12 gap-8 bg-brand-senary mt-[84px]">
-            <div className="col-span-3 col-start-">
+            <div className="col-span-3 col-start flex items-center mx-auto">
+
               <Image
                 width={275}
                 height={60}
@@ -59,7 +65,8 @@ const Footer = () => {
                 alt="Logo Footer"
               />
             </div>
-            <div className="flex flex-col">
+            <div className="col-span-3 col-start-5">
+
               <p>Artmazigh studios</p>
               <p>
                 160 Robinson Road, #14-04 Singapore Business Federation Centre -
@@ -67,15 +74,21 @@ const Footer = () => {
               </p>
             </div>
             <Link href={'/contact-us'}>
-              <a>Contact Us</a>
+              <a className="flex col-span-2 text-center justify-center items-center">Contact Us</a>
             </Link>
-            <div className="list">
-              <div className="facebook"></div>
-              <div className="ig"></div>
-              <div className="fiver"></div>
+            <div className="col-span-3 col-end-13 flex items-center justify-end">
+              <div className="flex flex-row space-x-8 ">
+                <FacebookIcons className="w-11 h-11" />
+                <InstagramIcons className="w-11 h-11" />
+                <FiverrIcons className="w-11 h-11" />
+              </div>
             </div>
           </div>
         </Container>
+      </div>
+
+      <div className="grid grid-cols-12 max-w-screen-xl text-sm gap-8 w-full px-5 mx-auto relative h-[67px] items-center">
+          <p className="col-start-1 col-span-3">Design by: Mindhunter Studios @danyasary</p>
       </div>
     </>
   )
